9b34462715dafe54793477cf8ce98f9f5fa98489
ajit waman <ajitwaman353@gmail.com>
2025-09-08 10:44:50 +0530

    .env error solved

--- src/prisma/migrations/20250908051359_init/migration.sql
+++ src/prisma/migrations/20250908051359_init/migration.sql
@@ -1,10 +1,13 @@
+-- CreateEnum
+CREATE TYPE "public"."genre" AS ENUM ('funny', 'action', 'love', 'crime');
+
 -- CreateTable
 CREATE TABLE "public"."movie" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "director" TEXT NOT NULL,
     "releaseYear" INTEGER NOT NULL,
-    "genre" TEXT NOT NULL,
+    "genre" "public"."genre" NOT NULL,
     "rating" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "movie_pkey" PRIMARY KEY ("id")
